60013437bc81a644ebfcb04cb84f52755d373443
lishu213112-maker <fato213112@gmail.com>
2026-06-20 11:22:25 +0500

    Rename game.html to index.html

--- dom.js
+++ dom.js
@@ -1,242 +1,26 @@
-<<<<<<< HEAD
+let count = 0
+
+const countText = document.getElementById("count")
+
+const plusBtn = document.getElementById("plus")
+
+const minusBtn = document.getElementById("minus")
 
 
+plusBtn.addEventListener("click", function(){
 
+    count++
 
-
-
-{
-let count=0;
-let counttext=document.getElementById("count");
-let increament=document.getElementById("increasebtn");
-let decreament=document.getElementById("decreasebtn");
-increament.addEventListener("click",function(){
-    count++;
-    counttext.textContent=count;
-});
-decreament.addEventListener("click",function(){
-    count--;
-    counttext.textContent=count;
-});
-}
-=======
-// new code
-
-let cells = document.querySelectorAll(".cell")
-
-let statusText = document.querySelector("#status")
-
-let restartBtn = document.querySelector("#button")
-
-let currentPlayer = "X"
-
-let gameRunning = true
-
-
-let winPatterns = [
-
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-
-    [0,4,8],
-    [2,4,6]
-
-]
-
-
-
-cells.forEach(function(cell){
-
-    cell.addEventListener("click", cellClicked)
+    countText.textContent = count
 
 })
 
 
+minusBtn.addEventListener("click", function(){
 
-function cellClicked(){
+    count--
 
+    countText.textContent = count
 
-    if(this.textContent != "" || !gameRunning){
+})
 
-        return
-
-    }
-
-
-    this.textContent = currentPlayer
-
-
-
-    if(currentPlayer == "X"){
-
-        this.style.color="light"
-
-    }
-
-    else{
-
-        this.style.color = "hotpink"
-
-    }
-
-
-
-    checkWinner()
-
-}
-
-
-
-function checkWinner(){
-
-    let roundWon = false
-
-
-
-    for(let i = 0; i < winPatterns.length; i++){
-
-
-        let pattern = winPatterns[i]
-
-
-        let cell1 = cells[pattern[0]].textContent
-        let cell2 = cells[pattern[1]].textContent
-        let cell3 = cells[pattern[2]].textContent
-
-
-
-        if(cell1 == "" || cell2 == "" || cell3 == ""){
-
-            continue
-
-        }
-
-
-
-        if(cell1 == cell2 && cell2 == cell3){
-
-
-            cells[pattern[0]].style.backgroundColor = "lime"
-
-            cells[pattern[1]].style.backgroundColor = "lime"
-
-            cells[pattern[2]].style.backgroundColor = "lime"
-
-
-            roundWon = true
-
-            break
-
-        }
-
-    }
-
-
-
-    if(roundWon){
-
-        statusText.textContent = `Player ${currentPlayer} Wins`
-
-        statusText.style.color = "lime"
-
-        gameRunning = false
-
-        return
-
-    }
-
-
-
-    let filled = true
-
-
-
-    cells.forEach(function(cell){
-
-        if(cell.textContent == ""){
-
-            filled = false
-
-        }
-
-    })
-
-
-
-    if(filled){
-
-        statusText.textContent = "Match Draw"
-
-        statusText.style.color = "orange"
-
-        gameRunning = false
-
-        return
-
-    }
-
-
-
-    changePlayer()
-
-}
-
-
-
-function changePlayer(){
-
-
-    if(currentPlayer == "X"){
-
-        currentPlayer = "O"
-
-    }
-
-    else{
-
-        currentPlayer = "X"
-
-    }
-
-
-
-    statusText.textContent = `Player ${currentPlayer} Turn`
-
-}
-
-
-
-button.addEventListener("click", restartGame)
-
-
-
-function restartGame(){
-
-
-    cells.forEach(function(cell){
-
-
-        cell.textContent = ""
-
-        cell.style.backgroundColor = ""
-
-    })
-
-
-
-    currentPlayer = "X"
-
-    gameRunning = true
-
-
-    statusText.textContent = "Player X Turn"
-
-    statusText.style.color = "white"
-
-}
->>>>>>> 5aae8094388313bfdd5084187966400bf83eaa83
